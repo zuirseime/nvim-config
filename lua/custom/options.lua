0113e8e402f745808ae5629e0b281110b2e3231e
zuirseime <vovakibets88@gmail.com>
2026-01-27 18:21:58 +0200

    feat: structuralized vim options

--- lua/custom/options.lua
+++ lua/custom/options.lua
@@ -1,53 +1,77 @@
 local opt = vim.opt
 local g = vim.g
-local o = vim.o
 
 vim.cmd [[colorscheme everforest]]
 
+-- UI
 opt.number = true
 opt.relativenumber = true
+opt.cursorline = true
+opt.cursorlineopt = 'both'
+opt.showmode = false
+opt.signcolumn = 'yes'
+opt.scrolloff = 10
+opt.confirm = true
+
+-- Indentation
 opt.shiftwidth = 4
 opt.tabstop = 4
 opt.expandtab = true
-opt.cursorline = true
-opt.mouse = ''
 opt.smartindent = true
-opt.spelllang = { 'en', 'uk' }
-opt.virtualedit = 'block'
+opt.breakindent = true
+
+-- Text Rendering
 opt.wrap = false
+opt.virtualedit = 'block'
+opt.list = true
 opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
+-- Search
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.inccommand = 'split'
+
+-- Splits & Windows
+opt.splitright = true
+opt.splitbelow = true
+
+-- Performance
+opt.updatetime = 250
+opt.timeoutlen = 300
+
+-- Files
+opt.undofile = true
+opt.swapfile = false
+opt.backup = false
+opt.writebackup = false
+
+-- Input
+opt.mouse = ''
+opt.backspace = { 'indent', 'eol', 'start' }
+opt.formatoptions = 'rjqn'
+
+-- Completion & Command-Line
+opt.completeopt = { 'menu', 'menuone', 'noselect' }
+opt.pumheight = 10
+
+opt.shortmess = 'aoOsTAC'
+opt.cmdheight = 0
+
+-- Spellchecking
+opt.spelllang = { 'en', 'uk' }
+
+-- Filetype & Encoding Sefety
+opt.encoding = 'utf-8'
+opt.fileencoding = 'utf-8'
+
+-- Globals
 g.ai_cmp = true
 g.base46_cache = vim.fn.stdpath 'data' .. '/base46/'
 g.have_nerd_font = false
 
-o.cursorline = true
-o.cursorlineopt = 'both'
-o.showmode = false
-
+-- Misc
 vim.schedule(function()
-  o.clipboard = 'unnamedplus'
+  opt.clipboard = 'unnamedplus'
 end)
-
-o.breakindent = true
-o.undofile = true
-
-o.ignorecase = true
-o.smartcase = true
-
-o.signcolumn = 'yes'
-
-o.updatetime = 250
-
-o.timeoutlen = 300
-
-o.splitright = true
-o.splitbelow = true
-
-o.list = true
-
-o.inccommand = 'split'
-
-o.scrolloff = 10
-
-o.confirm = true
